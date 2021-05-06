d05b79a0a3fcc09cce4f99c0a591b226a2b81b37
HIMANSHI2121 <77221084+HIMANSHI2121@users.noreply.github.com>
2021-05-06 22:03:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ function preload(){
 }
 function setup() {
   createCanvas(1200,650);
-  r1 = createSprite(400,630,370,50);
+  /*r1 = createSprite(400,630,370,50);
   r1.shapeColor = "green";
   //r1.addImage(bg);
   //r1.scale = 0.7;
@@ -35,42 +35,107 @@ function setup() {
 
 function draw() {
   background(backG);
-  for(var i=600; i<650; i=i+10){
-    fill("white");
-    rect(600,i,10,5);
+  //vertical:::
+  for(var i=605; i<650; i=i+20){
+    stroke("darkCyan");
+    line(610,i,610,i+10);
   }
+  for(var i=0; i<70; i=i+20){
+    stroke("yellowGreen");
+    line(610,i,610,i+10);
+  }
+  for(var i=0; i<140; i=i+20){
+    stroke("darkCyan");
+    line(174,i,174,i+10);
+  }
+  for(var i=205; i<345; i=i+20){
+    stroke("black");
+    line(174,i,174,i+10);
+  }
+  for(var i=550; i<650; i=i+20){
+    stroke("darkCyan");
+    line(174,i,174,i+10);
+  }
+  for(var i=0; i<60; i=i+20){
+    stroke("darkCyan");
+    line(1020,i,1020,i+10);
+  }
+  for(var i=100; i<325; i=i+20){
+    stroke("darkCyan");
+    line(1020,i,1020,i+10);
+  }
+  for(var i=440; i<565; i=i+20){
+    stroke("darkCyan");
+    line(1020,i,1020,i+10);
+  }
+  for(var i=165; i<325; i=i+20){
+    stroke("darkCyan");
+    line(727,i,727,i+10);
+  }
+  for(var i=375; i<570; i=i+20){
+    stroke("darkCyan");
+    line(727,i,727,i+10);
+  }
+  //horizontal:::
   for(var i=200; i<550; i=i+20){
-    stroke("white");
+    stroke("blue");
     line(i,592,i+10,592);
   }
   for(var i=670; i<980; i=i+20){
-    stroke("white");
+    stroke("green");
     line(i,592,i+10,592);
   }
   for(var i=0; i<470; i=i+20){
-    stroke("white");
+    stroke("red");
     line(i,430,i+10,430);
   }
   for(var i=800; i<1200; i=i+20){
-    stroke("white");
+    stroke("yellow");
     line(i,345,i+10,345);
   }
   for(var i=207; i<950; i=i+20){
-    stroke("white");
+    stroke("crimson");
     line(i,80,i+10,80);
   }
   for(var i=350; i<730; i=i+20){
-    stroke("white");
+    stroke("orange");
     line(i,165,i+10,165);
   }
   for(var i=1050; i<1200; i=i+20){
-    stroke("white");
+    stroke("purple");
     line(i,165,i+10,165);
   }
   for(var i=1030; i<1200; i=i+20){
-    stroke("white");
+    stroke("seaGreen");
     line(i,430,i+10,430);
   }
- 
+  for(var i=000; i<175; i=i+20){
+    stroke("brown");
+    line(i,345,i+10,345);
+  }
+  for(var i=000; i<140; i=i+20){
+    stroke("cyan");
+    line(i,166,i+10,166);
+  }
+  for(var i=000; i<175; i=i+20){
+    stroke("goldenRod");
+    line(i,550,i+10,550);
+  }
+  for(var i=1070; i<1200; i=i+20){
+    stroke("midnightBlue");
+    line(i,550,i+10,550);
+  }
+  for(var i=1030; i<1200; i=i+20){
+    stroke("red");
+    line(i,430,i+10,430);
+  }
+  for(var i=400; i<520; i=i+20){
+    stroke("cyan");
+    line(i,260,i+10,260);
+  }
+  for(var i=200; i<280; i=i+20){
+    stroke("cyan");
+    line(i,260,i+10,260);
+  }
   drawSprites();
 }
