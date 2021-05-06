ea5f238d1c71d1deabda3c749f782ac01fccc329
HIMANSHI2121 <77221084+HIMANSHI2121@users.noreply.github.com>
2021-05-06 20:54:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,27 +9,28 @@ function preload(){
 }
 function setup() {
   createCanvas(1200,650);
-  r1 = createSprite(405,630,370,50);
+  r1 = createSprite(400,630,370,50);
   r1.shapeColor = "green";
   //r1.addImage(bg);
   //r1.scale = 0.7;
-  r2 = createSprite(770,630,240,70);
+  r2 = createSprite(770,630,240,50);
   r2.shapeColor = "blue";
-  r3 = createSprite(70,630,135,200);
+  r3 = createSprite(70,630,125,200);
   r3.shapeColor = "red";
-  r4 = createSprite(1100,630,185,200);
+  r4 = createSprite(1100,630,125,200);
   r4.shapeColor = "yellow";
-  //r5 = createSprite(1100,630,185,200);
-  //r5.shapeColor = "green";
+  /*r5 = createSprite(1000,630,185,200);
+  //r5.shapeColor = "seaGreen";
   //r6 = createSprite(1100,630,185,200);
   //r6.shapeColor = "green";
-  //r7 = createSprite(1100,630,185,200);
-  //r7.shapeColor = "green";
-  //r8 = createSprite(1100,630,185,200);
-  //r8.shapeColor = "green";
+  //r7 = createSprite(1100,100,185,200);
+  //r7.shapeColor = "yellow";
+  //r8 = createSprite(1100,60,185,200);
+  //r8.shapeColor = "darkGreen";
+  //r9 = createSprite(300,200,10,40);
   truck = createSprite(400, 200, 50, 50);
   truck.addImage(truckImg);
-  truck.scale = 0.7;
+  truck.scale = 0.7;*/
 }
 
 function draw() {
@@ -38,5 +39,38 @@ function draw() {
     fill("white");
     rect(600,i,10,5);
   }
+  for(var i=200; i<550; i=i+20){
+    stroke("white");
+    line(i,592,i+10,592);
+  }
+  for(var i=670; i<980; i=i+20){
+    stroke("white");
+    line(i,592,i+10,592);
+  }
+  for(var i=0; i<470; i=i+20){
+    stroke("white");
+    line(i,430,i+10,430);
+  }
+  for(var i=800; i<1200; i=i+20){
+    stroke("white");
+    line(i,345,i+10,345);
+  }
+  for(var i=207; i<950; i=i+20){
+    stroke("white");
+    line(i,80,i+10,80);
+  }
+  for(var i=350; i<730; i=i+20){
+    stroke("white");
+    line(i,165,i+10,165);
+  }
+  for(var i=1050; i<1200; i=i+20){
+    stroke("white");
+    line(i,165,i+10,165);
+  }
+  for(var i=1030; i<1200; i=i+20){
+    stroke("white");
+    line(i,430,i+10,430);
+  }
+ 
   drawSprites();
 }
